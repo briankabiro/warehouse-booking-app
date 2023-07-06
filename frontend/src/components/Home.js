52580b72dc7499be9de2fc6b482ad629d2e3e781
Brian Kabiro <briankabiro@gmail.com>
2023-07-06 03:34:49 +0300

    Add prettier package and fix linting errors

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,9 +1,9 @@
-import SlotsComponent from './SlotsComponent';
-import React, { useState } from 'react';
-import SlotsCalendar from './Slots/SlotsCalendar';
-import { PlusOutlined } from '@ant-design/icons';
-import { Button, Modal } from 'antd';
-import NavBar from './NavBar/NavBar';
+import SlotsComponent from "./SlotsComponent";
+import React, { useState } from "react";
+import SlotsCalendar from "./Slots/SlotsCalendar";
+import { PlusOutlined } from "@ant-design/icons";
+import { Button, Modal } from "antd";
+import NavBar from "./NavBar/NavBar";
 
 const Home = () => {
   const [getSlotsModalOpen, setGetSlotsModalOpen] = useState(false);
@@ -23,20 +23,31 @@ const Home = () => {
   return (
     <>
       <NavBar />
-      
-      <div id='new-booking-section'>
-        <Button onClick={showModal} id='new-booking-button' size='large' type='primary' icon={<PlusOutlined />}>
+
+      <div id="new-booking-section">
+        <Button
+          onClick={showModal}
+          id="new-booking-button"
+          size="large"
+          type="primary"
+          icon={<PlusOutlined />}
+        >
           New Booking
         </Button>
       </div>
-      
+
       <SlotsCalendar />
 
-      <Modal title="Search Available Slots" open={getSlotsModalOpen} onOk={handleOk} onCancel={handleCancel}>
+      <Modal
+        title="Search Available Slots"
+        open={getSlotsModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
         <SlotsComponent />
       </Modal>
     </>
-  )
-}
+  );
+};
 
 export default Home;
